d86f4cd76c3fe3f4758d1986a3608c5acabac308
yoonewon <57882279+yoonewon@users.noreply.github.com>
2023-10-09 22:26:10 +0900

    Create index.js

---
+++ HW/Chapter3/Exercise3.1/index.js
@@ -0,0 +1,16 @@
+const express = require(`express`);
+const port =3000;
+const app =express();
+app.use(express.urlencoded({extended : true}));
+
+const objectToString = obj => Object.keys(obj).map(k => `${k}: ${obj[k]}`).join(`\n`);
+
+app.get('/',(req,res) => res.send(objectToString(req.query)));
+
+app.post('/',(req,res) => res.send(objectToString(req.body)));
+
+app.put('/',(req,res) => res.send(objectToString(req.body)));
+
+app.delete('/',(req,res) => res.send(objectToString(req.body)));
+
+app.listen(port, () => console.log(`Server listening on port ${port}!`));
